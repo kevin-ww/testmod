3b167077155f4836eb460afc20f9b81f0f0cee3f
Kevin Wu <kevin.wu@magicwindow.cn>
2019-06-03 21:38:27 +0800

    change hi to allow multilang

--- app.go
+++ app.go
@@ -1,8 +1,29 @@
 package testmod
 
-import "fmt"
+// import "fmt"
 
-// Hi returns a friendly greeting
-func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s , this is v1.0.1,done by kevin", name)
+// // Hi returns a friendly greeting
+// func Hi(name string) string {
+// 	return fmt.Sprintf("Hi, %s , this is v1.0.1,done by kevin", name)
+// }
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Hi returns a friendly greeting in language lang
+func Hi(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name), nil
+	case "pt":
+		return fmt.Sprintf("Oi, %s!", name), nil
+	case "es":
+		return fmt.Sprintf("¡Hola, %s!", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour, %s!", name), nil
+	default:
+		return "", errors.New("unknown language")
+	}
 }
